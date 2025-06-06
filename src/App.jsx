a5791ba31ec4f860fc7383773142337ff7136d03
skajimuddin <skazimuddin7786@gmail.com>
2025-06-07 01:12:46 +0530

    refactor routing structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./Pages/HomePage"
 
 function App() {
   return (
     <>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<h1>404</h1>} />
+          <Route path="/" element={<HomePage />} /> 
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
